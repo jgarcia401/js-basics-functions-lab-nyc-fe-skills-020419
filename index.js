fcc23acf1364afae7c94962357ed9c35e82cc268
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-17 17:06:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function calculatesFarePrice(start, destination) {
   if (distanceTravelledInFeet(start, destination) < 400) {
     
     return 0 
-  } else if ()
+  } else if (distanceTravelledInFeet(start, destination) > 400)
   
   }
