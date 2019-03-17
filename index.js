0b6d3a6511a920b5d24a613fd22f46520b1e4c1b
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-17 16:51:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+const headquartersBlock = 42
+
+const feetPerBlock = 264
+
+function distanceFromHqInBlocks(block) {
+  
+  return Math.abs(block - headquartersBlock)
+  
+  
+}
+
+function distanceFromHqInFeet(block) {
+  
+  return distanceFromHqInBlocks(block) * feetPerBlock
+  
+}
+
+function distanceTraveledInFeet(block1, block2) {
+  
+  return Math.abs((block1 * feetPerBlock) - (block2 * feetPerBlock))
+}
+
